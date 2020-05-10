60f45e3e487e1980f8a9a690a861654b85364d19
Chris Santamaria <chris@santamaria.me>
2020-05-10 16:34:42 -0400

    Remove unused class names

--- packages/cra-template-minimal-typescript/template/src/App.tsx
+++ packages/cra-template-minimal-typescript/template/src/App.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App" style={{ textAlign: 'center' }}>
-      <header className="App-header">
+    <div style={{ textAlign: 'center' }}>
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
